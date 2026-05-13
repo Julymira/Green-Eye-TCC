9d1fc950837669a95e7074f8b6ad359283558e16
Julyana Mira <julyanamira@gmail.com>
2026-05-13 16:39:27 -0300

    🐛 fix: Corrige altura do mapa nas páginas de nova ocorrência

--- frontend/src/pages/public/NovaOcorrencia.js
+++ frontend/src/pages/public/NovaOcorrencia.js
@@ -150,7 +150,7 @@ function NovaOcorrencia() {
                         <MapContainer
                             center={[-16.2531, -47.9503]}
                             zoom={14}
-                            style={{ height: '100%', width: '100%' }}
+                            style={{ height: '500px', width: '100%' }}
                         >
                             <TileLayer
                                 url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
